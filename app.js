d10ca718d954596ec0de9a126a9132a15fbb60c7
tim hopp <timothyjhopp@gmail.com>
2020-06-27 15:47:23 -0600

    improved style & function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let collectionInterval 
 
 let cheese = {
-  count: 50000,
+  count: 20000,
   shovelCount: 0,
   oxCount: 0,
   tractorCount: 0,
@@ -30,7 +30,7 @@ let automaticUpgrade = {
    potatoMobile: {
      price: 10000,
      quantity: 0,
-     multiplier: 300,
+     multiplier: 300
    }
 }
 
@@ -43,39 +43,84 @@ function mine(){
 cheese.count += (1 + (cheese.shovelCount*clickUpgrade.shovel.multiplier) + (cheese.oxCount*clickUpgrade.ox.multiplier))
  console.log(cheese)
  drawCheese()
+ drawPotato()
 }
 
 function drawCheese(){
   let template = '';
     template += `
-    <div class="text-center card card bg-purp">
-        <h3>Inventory</h3>
-        <p>Cheese: ${cheese.count} </p>
-        <p>Ox & Plow: ${cheese.oxCount}</p>
-        <p>Tractor: ${cheese.tractorCount}</p>
-        <P>Potato Mobile: ${cheese.potatoCount}</p>
+    <div class="text-center text-warning card card bg-purp p-4">
+        <h2>Inventory</h2>
+        <h5>Shovel : ${cheese.shovelCount}</h5>
+        <h5>Ox & Plow : ${cheese.oxCount}</h5>
+        <h5>Tractor : ${cheese.tractorCount}</h5>
+        <h5>Potato Mobile : ${cheese.potatoCount}</h5>
       </div>
     `
   document.getElementById('inventory').innerHTML= template
 }
 
+function drawPotato(){
+  let template = '';
+  template += `
+  <div class="row card bg-purp">
+  <img class = "potatoImg mx-auto my-auto" src="https://i.pinimg.com/originals/dd/8a/41/dd8a410fb404f776e3cad0927c5e809e.png">
+</div>
+<div class="row justify-content-center card bg-purp">
+  <h4 class="text-center text-warning">Potato Count: ${cheese.count}</h4>
+</div>
+ `
+
+ document.getElementById('potato').innerHTML= template
+}
+
 function drawAuto(){
   let template = '';
     template += `
-    <div class="text-center card bg-purp">
-        <h3>Stats</h3>
-        <p>CPS: ${cheeseModifier.cPS} </p>
-        <p>TCM: ${cheeseModifier.tCM}</p>
+    <div class="text-center card text-warning bg-purp p-4">
+        <h2>Stats</h2>
+        <h5>Potatoes Per Second : ${cheeseModifier.cPS} </h5>
+        <h5>Total Potato Multiplier : ${cheeseModifier.tCM}</h5>
       </div>
 
     `
   document.getElementById('stats').innerHTML= template
+  drawPotato()
+}
+
+function drawStore(){
+  let template= '';
+   template += `
+   <div class="col-12 p-4 card bg-purp">
+          <h2 class="text-warning text-center p-2">Store</h2>
+    
+            <div class="row justify-content-center pb-4">
+              <button classId= 'shovel' class="btn btn-warning" onclick="buyShovel()">Shovel</button>
+              <h4 class="my-auto text-warning pl-1 pr-5"> = ${clickUpgrade.shovel.price}</h4>
+              <button classId= 'ox' class="btn btn-warning" onclick="buyOx()">Ox & Plow</button>
+              <h4 class="my-auto text-warning pl-1"> = ${clickUpgrade.ox.price}</h4>
+            </div>
+        
+          <div class="row justify-content-center pb-4">
+            <button classId= 'tractor' class="btn btn-warning" onclick="buyTractor()">Tractor</button>
+            <h4 class="my-auto text-warning pl-1 pr-5"> = ${automaticUpgrade.tractor.price}</h4>
+    
+            <button classId= 'potatoMobile' class="btn btn-warning" onclick='buyPotato()'>Potato Mobile!</button>
+            <h4 class="my-auto text-warning pl-1"> = ${automaticUpgrade.potatoMobile.price}</h4>
+          </div>
+          
+        </div>
+   `
+   document.getElementById('store').innerHTML = template
+   drawPotato()
+
 }
 
 function buyShovel(){
   if(cheese.count >= 50){
     cheese.count -= 50;
     cheese.shovelCount++
+    clickUpgrade.shovel.price += 50;
     cheeseModifier.tCM += 2;
     alert('Shovel Purchased')
   } else {
@@ -83,12 +128,14 @@ function buyShovel(){
   }
   drawCheese()
   drawAuto()
+  drawStore()
 }
 
 function buyOx(){
   if(cheese.count >= 150){
     cheese.count -= 150;
     cheese.oxCount++
+    clickUpgrade.ox.price += 150;
     cheeseModifier.tCM += 10;
     alert('Ox & Plow Purchased')
   } else {
@@ -96,6 +143,7 @@ function buyOx(){
   }
   drawCheese()
   drawAuto()
+  drawStore()
 }
 
 
@@ -106,9 +154,11 @@ function buyTractor(){
     cheeseModifier.tCM += 30;
     cheeseModifier.cPS += 10;
     automaticUpgrade.tractor.quantity++
+    automaticUpgrade.tractor.price += 500;
     alert('Tractor Purchased')
     drawCheese()
     drawAuto()
+    drawStore()
   } else {
     alert('You dont have enough money')
   }
@@ -121,10 +171,12 @@ function buyPotato(){
     cheese.potatoCount++
     cheeseModifier.tCM += 300;
     cheeseModifier.cPS += 100;
+    automaticUpgrade.potatoMobile.price += 10000;
     automaticUpgrade.potatoMobile.quantity++
     alert('Potato Purchased')
     drawCheese()
     drawAuto()
+    drawStore()
   } else {
     alert('You dont have enough money')
   }
@@ -140,6 +192,7 @@ function collectAutoUpgrades(){
   }
   drawCheese()
   drawAuto()
+  drawPotato()
 }
 
 function startInterval(){
@@ -148,3 +201,5 @@ function startInterval(){
 
 drawCheese()
 drawAuto()
+drawStore()
+drawPotato()
--- test.js
+++ test.js
@@ -1,27 +1,3 @@
-function buyShovel(){
-  if(cheese.count > 50){
-    cheese.count -= 50;
-    cheese.shovelCount++
-    cheeseModifier.tCM += 2;
-    alert('Shovel Purchased')
-  } else {
-    alert('You dont have enough money')
-  }
-  drawCheese()
-  drawAuto()
-}
 
-function buyTractor(){
-  if(cheese.count > 500){
-    cheese.count -= 500;
-    cheese.tractorCount++
-    cheeseModifier.tCM += 20;
-    automaticUpgrade.tractor.quantity++
-    alert('Tractor Purchased')
-    drawCheese()
-    drawAuto()
-  } else {
-    alert('You dont have enough money')
-  }
-  startInterval()
-}
+          
+        
