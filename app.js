cd5a62cb2ed89a64a9e3a2aa4261d005499fcea4
tim hopp <timothyjhopp@gmail.com>
2020-07-05 16:32:40 -0600

    Improved function

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//* 0-Potato Count 1-Shovel 2-Ox 3-Tractor 4-Potato
 let cheese = {
-  count: 300,
-  shovelCount: 0,
-  oxCount: 0,
-  tractorCount: 0,
-  potatoCount: 0,
+  potato: 0,
+  shovel: 0,
+  ox: 0,
+  tractor: 0,
+  potatoMobile: 0,
 }
 
 let clickUpgrade = {
@@ -38,39 +39,37 @@ let cheeseModifier = {
 }
 
 function mine(){
-cheese.count += (1 + (cheese.shovelCount*clickUpgrade.shovel.multiplier) + (cheese.oxCount*clickUpgrade.ox.multiplier));
- drawCheese()
- drawPotato()
- drawStore()
+cheese.potato += (1 + (cheese.shovel *clickUpgrade.shovel.multiplier) + (cheese.ox *clickUpgrade.ox.multiplier));
+ drawElements()
+ awards()
 }
 
-function drawCheese(){
+function drawElements(){
+  //*draw inventory
   let template = '';
     template += `
     <div class="text-center text-warning card card bg-purp p-4">
         <h2>Inventory</h2>
-        <h5>Shovel : ${cheese.shovelCount}</h5>
-        <h5>Ox & Plow : ${cheese.oxCount}</h5>
-        <h5>Tractor : ${cheese.tractorCount}</h5>
-        <h5>Potato Mobile : ${cheese.potatoCount}</h5>
+        <h5>Shovel : ${cheese.shovel}</h5>
+        <h5>Ox & Plow : ${cheese.ox}</h5>
+        <h5>Tractor : ${cheese.tractor}</h5>
+        <h5>Potato Mobile : ${cheese.potatoMobile}</h5>
       </div>
     `
   document.getElementById('inventory').innerHTML= template
-}
 
-function drawPotato(){
-  let template = '';
+ //*draw potato count
+  template = '';
   template += `
   <div class="row mx-auto my-auto">
    <img id = "potatoImg" class="potatoImg mx-auto my-auto potatoShadow" src="https://i.pinimg.com/originals/dd/8a/41/dd8a410fb404f776e3cad0927c5e809e.png">
-  <h2 class=" potatoText text-center text-warning pt-5">${cheese.count}</h2>
+  <h2 class="potatoText text-center text-warning pt-5">${cheese.potato}</h2>
   </div>
  `
  document.getElementById('potato').innerHTML= template
-}
 
-function drawAuto(){
-  let template = '';
+ //*draw stats
+  template = '';
     template += `
     <div class="text-center card text-warning bg-purp p-4">
         <h2>Stats</h2>
@@ -80,11 +79,9 @@ function drawAuto(){
 
     `
   document.getElementById('stats').innerHTML= template
-  drawPotato()
-}
 
-function drawStore(){
-  let template= '';
+  //*draw store
+  template= '';
    template += `
    <div class="col-12 p-4 card bg-purp">
           <h2 class="text-warning text-center p-2">Store</h2>
@@ -103,45 +100,46 @@ function drawStore(){
             <button id= 'potatoMobile' class="btn btn-warning itemGone" disabled onclick='buyPotato()'>Potato Mobile!</button>
             <h4 class="my-auto text-warning pl-1"> = ${automaticUpgrade.potatoMobile.price}</h4>
           </div>
-          <div class="text-center">Total Spent : ${purchaseTotal}</div>
+          <div class="text-center text-warning">Total Spent : ${purchaseTotal}</div>
         </div>
    `
    document.getElementById('store').innerHTML = template
+
    btnControl()
    btnDisable()
-   drawPotato()
-  }
+}
 
 function btnDisable(){
-   if(cheese.count >= clickUpgrade.shovel.price){
+   if(cheese.potato >= clickUpgrade.shovel.price){
     document.getElementById('shovel').removeAttribute('disabled')
    }
-   if(cheese.count >= clickUpgrade.ox.price){
+   if(cheese.potato >= clickUpgrade.ox.price){
    document.getElementById('ox').removeAttribute('disabled')
   }
-   if(cheese.count >= automaticUpgrade.tractor.price){
+   if(cheese.potato >= automaticUpgrade.tractor.price){
     document.getElementById('tractor').removeAttribute('disabled')
    }
-   if(cheese.count >= automaticUpgrade.potatoMobile.price){
+   if(cheese.potato >= automaticUpgrade.potatoMobile.price){
     document.getElementById('potatoMobile').removeAttribute('disabled')
    }
 }
 
 //* Figure out how to only hide buttons till you were able to purchase it ONCE, even if you cannot anymore
 function btnControl(){
-  if(purchaseTotal>=50 || cheese.count>=50){
+  if(purchaseTotal>=50 || cheese.potato>=50){
     document.getElementById('shovel').classList.remove('itemGone')
    }
-   if(purchaseTotal>=150){
+   if(purchaseTotal>=150 || cheese.potato>=150){
     document.getElementById('ox').classList.remove('itemGone')
    }
-   if(purchaseTotal>=500){
+   if(purchaseTotal>=500 || cheese.potato>=500){
     document.getElementById('tractor').classList.remove('itemGone')
    }
-   if(purchaseTotal>=10000){
+   if(purchaseTotal>=10000 || cheese.potato>=10000){
     document.getElementById('potatoMobile').classList.remove('itemGone')
    }
 }
+
 let purchaseTotal = 0;
 function purchase(item){
   if(item == 'shovel'){
@@ -159,75 +157,82 @@ function purchase(item){
   console.log(purchaseTotal)
 }
 
+function awards(){
+  let firstCheck = 1;
+  let secCheck = 1;
+  let thirdCheck = 1;
+  let fourthCheck = 1;
+
+  if((purchaseTotal + cheese.potato) == 10000 && (firstCheck = 1)){
+    alert('Milestone Achieved: 10,000 Potatoes Harvested')
+    firstCheck--
+  }
+  if((purchaseTotal + cheese.potato) == 50000 && (secCheck = 1)){
+    alert('Milestone Achieved: 50,000 Potatoes Harvested')
+    secCheck--
+  }
+  if((purchaseTotal + cheese.potato) == 100000 && (thirdCheck = 1)){
+    alert('Milestone Achieved: 100,000 Potatoes Harvested')
+    thirdCheck--
+  }
+  if((purchaseTotal + cheese.potato) == 1000000  && (fourthCheck = 1)){
+    alert('Milestone Achieved: 1,000,000 Potatoes Harvested')
+    fourthCheck--
+  }
+}
+
 
 function buyShovel(){
-  if(cheese.count >= clickUpgrade.shovel.price){
+  if(cheese.potato >= clickUpgrade.shovel.price){
     purchase('shovel')
-    cheese.count -= clickUpgrade.shovel.price;
-    cheese.shovelCount++
-    cheese.total++
+    cheese.potato -= clickUpgrade.shovel.price;
+    cheese.shovel++
     clickUpgrade.shovel.price += 50;
     cheeseModifier.tCM += 2;
     alert('Shovel Purchased')
   } 
-  drawCheese()
-  drawAuto()
-  drawStore()
-  
+  drawElements()
 }
 
 function buyOx(){
-  if(cheese.count >= clickUpgrade.ox.price){
+  if(cheese.potato >= clickUpgrade.ox.price){
     purchase('ox')
-    cheese.count -= clickUpgrade.ox.price;
-    cheese.oxCount++
-    cheese.total++
+    cheese.potato -= clickUpgrade.ox.price;
+    cheese.ox++
     clickUpgrade.ox.price += 150;
     cheeseModifier.tCM += 10;
     alert('Ox & Plow Purchased')
   } 
-  drawCheese()
-  drawAuto()
-  drawStore()
-  
+  drawElements()
 }
 
 
 function buyTractor(){
-  if(cheese.count >= automaticUpgrade.tractor.price){
+  if(cheese.potato >= automaticUpgrade.tractor.price){
     purchase('tractor')
-    cheese.count -= automaticUpgrade.tractor.price;
-    cheese.tractorCount++
-    cheese.total++
+    cheese.potato -= automaticUpgrade.tractor.price;
+    cheese.tractor++
     cheeseModifier.tCM += 30;
     cheeseModifier.cPS += 10;
     automaticUpgrade.tractor.quantity++
     automaticUpgrade.tractor.price += 500;
     alert('Tractor Purchased')
-    drawCheese()
-    drawAuto()
-    drawStore()
-    
   } 
-  
+  drawElements()
 }
 
 function buyPotato(){
-  if(cheese.count >= automaticUpgrade.potatoMobile.price){
+  if(cheese.potato >= automaticUpgrade.potatoMobile.price){
     purchase('potatoMobile')
-    cheese.count -= automaticUpgrade.potatoMobile.price;
-    cheese.potatoCount++
-    cheese.total++
+    cheese.potato -= automaticUpgrade.potatoMobile.price;
+    cheese.potatoMobile++
     cheeseModifier.tCM += 300;
     cheeseModifier.cPS += 100;
     automaticUpgrade.potatoMobile.price += 10000;
     automaticUpgrade.potatoMobile.quantity++
     alert('Potato Purchased')
-    drawCheese()
-    drawAuto()
-    drawStore()
-    
   } 
+  drawElements()
 }
 
 
@@ -235,20 +240,14 @@ function buyPotato(){
 function collectAutoUpgrades(){
   for(let key in automaticUpgrade){
     let keyLime = automaticUpgrade[key];
-    cheese.count += (keyLime.quantity * keyLime.multiplier)
+    cheese.potato += (keyLime.quantity * keyLime.multiplier)
   }
-  drawCheese()
-  drawAuto()
-  drawPotato()
-  drawStore()
+  drawElements()
 }
 
 function startInterval(){
   collectionInterval = setInterval(collectAutoUpgrades, 3000) 
 }
 
-drawCheese()
-drawAuto()
-drawStore()
-drawPotato()
+drawElements()
 startInterval()
